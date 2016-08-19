df6a63a6cb998bfde24b71477c63c14a4ea4dfae
Manuel Stritt <manuel.stritt@actelion.com>
2016-08-19 15:49:36 +0200

    updates

--- src/main/java/com/actelion/research/orbit/imageAnalysis/utils/OrbitUtils.java
+++ src/main/java/com/actelion/research/orbit/imageAnalysis/utils/OrbitUtils.java
@@ -50,7 +50,7 @@ import java.util.*;
 import java.util.List;
 
 public class OrbitUtils {
-    // label:  OrbitImageAnalysis226
+    // label:  OrbitImageAnalysis230
     public static final String VERSION_STR = getVersion() + (ScaleoutMode.SCALEOUTMODE.get() ? "G" : "") + (OrbitUtils.DEVELOPMENTMODE ? " DEVELOPMENT" : "");
     public static final boolean DEVELOPMENTMODE = false;
     public static final boolean TILEMODE = false;
