db0302578e91966acffa1b2d64cfb4407f766dd6
Manuel Stritt <manuel.stritt@actelion.com>
2017-06-23 10:13:26 +0200

    slider color test

---
+++ src/main/java/com/actelion/research/orbit/imageAnalysis/components/SliderColorTest.java
@@ -0,0 +1,55 @@
+/*
+ *     Orbit, a versatile image analysis software for biological image-based quantification.
+ *     Copyright (C) 2009 - 2017 Actelion Pharmaceuticals Ltd., Gewerbestrasse 16, CH-4123 Allschwil, Switzerland.
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.actelion.research.orbit.imageAnalysis.components;
+
+import com.actelion.research.orbit.utils.RawUtilsCommon;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SliderColorTest extends JFrame {
+
+    public SliderColorTest()  {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame.setDefaultLookAndFeelDecorated(true);
+                try {
+                    UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteGlassLookAndFeel");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+                JSlider slider = new JSlider(0,100,30);
+
+                add(slider);
+                pack();
+                RawUtilsCommon.centerComponent(SliderColorTest.this);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SliderColorTest sliderTest = new SliderColorTest();
+        sliderTest.setVisible(true);
+    }
+
+
+}
