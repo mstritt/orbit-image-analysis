cf8c8f0743024d24f2507f56350f76608f12ca44
fullejo1 <jonathan.fuller@idorsia.com>
2020-10-15 16:11:15 +0200

    Comment extra modules from settings.gradle. Update repository order.

--- src/main/java/com/actelion/research/orbit/imageAnalysis/tasks/GroovyExecutor.java
+++ src/main/java/com/actelion/research/orbit/imageAnalysis/tasks/GroovyExecutor.java
@@ -1,79 +1,53 @@
 /*
- *     Orbit, a versatile image analysis software for biological image-based quantification.
- *     Copyright (C) 2009 - 2018 Idorsia Pharmaceuticals Ltd., Hegenheimermattweg 91, CH-4123 Allschwil, Switzerland.
+ * Orbit, a versatile image analysis software for biological image-based quantification.
+ * Copyright (C) 2009 - 2020 Idorsia Pharmaceuticals Ltd., Hegenheimermattweg 91, CH-4123 Allschwil, Switzerland.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
 package com.actelion.research.orbit.imageAnalysis.tasks;
 
 import com.actelion.research.orbit.imageAnalysis.utils.OrbitLogAppender;
-import com.actelion.research.orbit.utils.RawUtilsCommon;
 import groovy.lang.GroovyShell;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.net.URL;
 
 public class GroovyExecutor {
     private static final Logger logger = LoggerFactory.getLogger(GroovyExecutor.class);
 
-    /*
-    public static void doTrustToCertificates() throws Exception {
-        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-        TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return null;
-                    }
-
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-                        return;
-                    }
-
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
-                        return;
-                    }
-                }
-        };
-
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        HostnameVerifier hv = new HostnameVerifier() {
-            public boolean verify(String urlHostName, SSLSession session) {
-                if (!urlHostName.equalsIgnoreCase(session.getPeerHost())) {
-                    System.out.println("Warning: URL host '" + urlHostName + "' is different to SSLSession host '" + session.getPeerHost() + "'.");
-                }
-                return true;
-            }
-        };
-        HttpsURLConnection.setDefaultHostnameVerifier(hv);
-    }
-    */
-
     public static void main(String[] args) throws Exception {
         OrbitLogAppender.GUI_APPENDER = false;
         if (args == null || args.length < 1)
             throw new IllegalArgumentException("No URL argument found. Call: GroovyExecutor <URL>");
-        // TODO: Do this properly...
-        //doTrustToCertificates();
-        //URL url = new URL("https://chiron.idorsia.com/stash/projects/ORBIT/repos/public-scripts/browse/QuantPerGroupLocalTest.groovy?at=5ff41667c494870aaacbe37fa43fd46fd4c9659c&raw");
+
         URL url = new URL(args[0]);
-        String content = RawUtilsCommon.getContentStr(url);
-        logger.debug("executing code:\n" + content);
+
+        InputStream in = url.openStream();
+        String content;
+        try {
+            System.out.println( IOUtils.toString( in ) );
+            content = IOUtils.toString( in );
+        } finally {
+            IOUtils.closeQuietly(in);
+        }
+
+        logger.info("executing code:\n" + content);
         logger.info("start executing groovy code");
         GroovyShell shell = new GroovyShell();
         shell.evaluate(content);
